684e4dda144a87c29fe0e56b15bd0799139b92c4
zainbits <hello.zain11@gmail.com>
2022-01-06 16:45:19 +0530

    fixed typewriter bug

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -4,17 +4,19 @@ import { useSelector } from 'react-redux'
 import { useTypewriter } from 'react-simple-typewriter'
 const Home = () => {
     const { name, qualities } = useSelector(state => state.contentful)
+
     const {text} = useTypewriter({
-        words: qualities,
+        words: qualities.length === 0 ? [''] : qualities,
         loop: 0, // Infinit
       })
+      
     return (
         <div className='home'>
             <div className='introduction'>
                 <div>Hi</div>
                 <div>My name is {name}</div>
                 <div>
-                    and i am a <span>{text}</span>
+                    and i am <span>{text}</span>
                 </div>
             </div>
         </div>
